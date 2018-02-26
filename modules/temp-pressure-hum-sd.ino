4895c79b1871b5b3d99578677dd5c3d042106339
lebedev Nikolay <n.lebedev@faunusaff.com>
2018-02-26 23:33:39 +0300

    refactor commit

--- modules/temp-pressure-hum-sd.ino
+++ modules/temp-pressure-hum-sd.ino
@@ -18,29 +18,25 @@
 #define MAXI 700
 #define PAUSE 500
 
+
 #define DEBUGMODE
+//#define LEDDEBUG
 
 //Serial port for logging. Use Hardware Serial  on Mega, or software serial for Nano
 #if defined DEBUGMODE
 #define SerialDEBUG Serial
 #endif
 
-/*Список, что переделат
-
-Все переменные.
-*/
 
 const uint8_t SD_SC = 10; //SD card 
 const uint8_t POWER_SWITCH = 5; //Turn on or turn off all sensor power via mosfet transistor
-const uint8_t INFO_LED = 13;
+const uint8_t ERRORLED = 13;
 
-
-File myFile,daily_file,one_file;
 
 unsigned long pressure, aver_pressure, pressure_array[6], time_array[6];
 unsigned long sumX, sumY, sumX2, sumXY;
 int delta,current_day;
-float a;
+float a,h,t,p;
 String check_data; //all sensor data in inflife line protocol
 
 
@@ -63,44 +59,21 @@ void setup() {
 
   //Включаем питание датчиков через транзистор
   pinMode(POWER_SWITCH, OUTPUT);
-  
-  #if defined DEBUGMODE
-  SerialDEBUG.print("Turn on power control via mosfet...");
-  #endif
 
   digitalWrite(POWER_SWITCH, HIGH);
   delay(100);
 
-  #if defined DEBUGMODE
-  SerialDEBUG.println("Done");
-  #endif
-
 
   //Включаем датчик температуры и влажности
-  
-  //startAndControl ("Turn on temp and humanity sensor...", dht.begin(), 1);
-
-  #if defined DEBUGMODE
-  SerialDEBUG.print("Turn on temp and humanity sensor...");
-  #endif
   dht.begin();
-  #if defined DEBUGMODE
-  SerialDEBUG.println("Done");
-  #endif
-
 
   //Включаем датчик давления
-
-  //startAndControl ("Turn on pressure sensor...", bmp.begin(), 1);
-  #if defined DEBUGMODE
-  SerialDEBUG.print("Turn on pressure sensor...");
-  #endif
   bmp.begin();
 
-  #if defined DEBUGMODE
-  SerialDEBUG.println("Done");
+  #if defined LEDDEBUG
+    errorFlash(ERRORLED,1);
   #endif
-  
+
   pressure = aver_sens();          // найти текущее давление по среднему арифметическому
   for (byte i = 0; i < 6; i++) {   // счётчик от 0 до 5
     pressure_array[i] = pressure;  // забить весь массив текущим давлением
@@ -111,125 +84,71 @@ void setup() {
 
 void loop() {
 
-  #if defined DEBUGMODE
-  SerialDEBUG.print("Turn on power control via mosfet...");
-  #endif
 
   digitalWrite(POWER_SWITCH, HIGH);
   delay(500);
 
-  #if defined DEBUGMODE
-  SerialDEBUG.println("Done");
-  #endif
-
   startAndControl ("Initializing sd...", SD.begin(SD_SC), 2);
+  //Read temperature
 
- //   if (!SD.begin(SD_SC)) {
- //   blinking(INFO_LED,MINI,MINI,MAXI,MAXI,PAUSE);
- // return;
- // }
-
-  bmp.readTemperature();  
-
-  delay(200);
+  startAndControl ("Read temperature from pressure sensor ...", bmp.readTemperature(), 3);
+  delay(100);
   
-  
-  if (RTC.read(tm)) {
-    delay(700);
-    }
-  else {
-    blinking(INFO_LED,MINI,MAXI,MAXI,MAXI,PAUSE);
-    return;
-    } 
+  //Read date time
+  startAndControl ("Read date time...", RTC.read(tm), 4);
+  delay(700);
   current_day = tm.Day;
    
-  //Считываем влажность
+  //Считываем влажность, температуру. давление
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+  p = bmp.readPressure();
   
-  float h = dht.readHumidity();
-  
-  // Считываем температуру и давление
-  
-  float t = dht.readTemperature();
-  float p = bmp.readPressure();
-  
-  
+   
   // Проверка удачно прошло ли считывание.
   
-  if (isnan(h) && isnan(t)&& isnan(p)) {
-  
-  blinking(INFO_LED,MAXI,MAXI,MAXI,MAXI,PAUSE);
-  return;
-  
+  startAndControl ("Check readed data from sensors...", isnan(h) && isnan(t)&& isnan(p), 8);
+
+  pressure = aver_sens();                          // найти текущее давление по среднему арифметическому
+  for (byte i = 0; i < 5; i++) {                   // счётчик от 0 до 5 (да, до 5. Так как 4 меньше 5)
+    pressure_array[i] = pressure_array[i + 1];     // сдвинуть массив давлений КРОМЕ ПОСЛЕДНЕЙ ЯЧЕЙКИ на шаг назад
+  }
+  pressure_array[5] = pressure;                    // последний элемент массива теперь - новое давление
+
+  sumX = 0;
+  sumY = 0;
+  sumX2 = 0;
+  sumXY = 0;
+  for (int i = 0; i < 6; i++) {                    // для всех элементов массива
+    sumX += time_array[i];
+    sumY += (long)pressure_array[i];
+    sumX2 += time_array[i] * time_array[i];
+    sumXY += (long)time_array[i] * pressure_array[i];
   }
 
-    pressure = aver_sens();                          // найти текущее давление по среднему арифметическому
-    for (byte i = 0; i < 5; i++) {                   // счётчик от 0 до 5 (да, до 5. Так как 4 меньше 5)
-      pressure_array[i] = pressure_array[i + 1];     // сдвинуть массив давлений КРОМЕ ПОСЛЕДНЕЙ ЯЧЕЙКИ на шаг назад
-    }
-    pressure_array[5] = pressure;                    // последний элемент массива теперь - новое давление
+  a = 0;
+  a = (long)6 * sumXY;             // расчёт коэффициента наклона приямой
+  a = a - (long)sumX * sumY;
+  a = (float)a / (6 * sumX2 - sumX * sumX);
+  delta = a * 6;                   // расчёт изменения давления
+  
+  check_data = "weather,location=uglovo,region=aerodrom temp="+String (t)+",hum="+String (h)+",pressure="+String (p)+",delta="+String (delta)+" "+String (makeTime(tm))+"000000000";
+  
+  write_to_sd ("file.txt", check_data, 9);
 
-    sumX = 0;
-    sumY = 0;
-    sumX2 = 0;
-    sumXY = 0;
-    for (int i = 0; i < 6; i++) {                    // для всех элементов массива
-      sumX += time_array[i];
-      sumY += (long)pressure_array[i];
-      sumX2 += time_array[i] * time_array[i];
-      sumXY += (long)time_array[i] * pressure_array[i];
-    }
-    a = 0;
-    a = (long)6 * sumXY;             // расчёт коэффициента наклона приямой
-    a = a - (long)sumX * sumY;
-    a = (float)a / (6 * sumX2 - sumX * sumX);
-    // Вопрос: зачем столько раз пересчитывать всё отдельными формулами? Почему нельзя считать одной большой?
-    // Ответ: а затем, что ардуинка не хочет считать такие большие числа сразу, и обязательно где-то наё*бывается,
-    // выдавая огромное число, от которого всё идёт по пи*зде. Почему с матами? потому что устал отлаживать >:O
-    delta = a * 6;                   // расчёт изменения давления
+  write_to_sd (String (current_day), check_data, 10);
+  
+  write_to_sd ("current.txt", check_data, 11);
 
-    myFile = SD.open("file.txt", FILE_WRITE);
-    delay(20);
-
-    check_data = "weather,location=uglovo,region=aerodrom temp="+String (t)+",hum="+String (h)+",pressure="+String (p)+",delta="+String (delta)+" "+String (makeTime(tm))+"000000000";
-    
-    // if the file opened okay, write to it:
-    if (myFile) {
-      myFile.println(check_data);
-      // close the file:
-      myFile.close();
-      
-      daily_file = SD.open(String (current_day), FILE_WRITE);
-      delay(20);
-      
-      if (current_day) {
-        daily_file.println(check_data);
-        // close the file:
-        daily_file.close();        
-        }
-      
-      one_file = SD.open("current.txt", FILE_WRITE);
-      delay(20);
-      
-      if (one_file) {
-        one_file.println(check_data);
-        // close the file:
-        one_file.close();        
-        }
-        
-    }
-    
-     else {
-      // if the file didn't open, print an error:
-      blinking(INFO_LED,MAXI,MAXI,MAXI,MAXI,5000);
-      return;
-    }
-  if ( tm.Hour == 23 and tm.Minute > 49 ) {
-  SD.remove("current.txt");        
+  if ( tm.Hour == 23 and tm.Minute > 30 ) {
+    startAndControl ("Remove current day file...", SD.remove("current.txt"), 12);
+     
   }
-      //turn off power via mosfet
-      delay(2000);
-      digitalWrite(POWER_SWITCH, LOW);
-      delay(600000);
+  
+  //turn off power via mosfet
+  delay(2000);
+  digitalWrite(POWER_SWITCH, LOW);
+  delay(5000);
    
   
   }
@@ -244,14 +163,6 @@ long aver_sens() {
   return aver_pressure;
 }
 
-void blinking (char led, unsigned short a, unsigned short b, unsigned short c, unsigned short d, unsigned short pause)
-{ 
-  pinMode(led, OUTPUT);
-  flash (led,a,pause);
-  flash (led,b,pause);
-  flash (led,c,pause);
-  flash (led,d,pause);
-}
 
 void flash (char led, unsigned short interval, unsigned short pause)
 {
@@ -271,6 +182,9 @@ void startAndControl (String message,boolean command,uint8_t count) {
         #if defined DEBUGMODE
         SerialDEBUG.println("Failed");
         #endif
+        #if defined LEDDEBUG
+          errorFlash(ERRORLED,count);        
+        #endif
         return;
        }
     else {
@@ -280,4 +194,49 @@ void startAndControl (String message,boolean command,uint8_t count) {
       #endif
       } 
    }
-  } 
+  }
+
+void flash (uint8_t led, uint8_t count, unsigned short interval, unsigned short pause)
+{ 
+  uint8_t  i;
+  
+  for (i=0; i < count; i++) {
+      digitalWrite(led, HIGH);
+      delay(interval);
+      digitalWrite(led, LOW);
+      delay(pause);     
+    }
+  
+ 
+  }
+
+void errorFlash (uint8_t errorled, uint8_t count)
+
+{   
+    flash(errorled,count,100,100);
+    digitalWrite(errorled, HIGH);
+    delay(2000);
+    digitalWrite(errorled, LOW);
+  }
+
+void write_to_sd (String filename, String check_data, uint8_t count) {
+  File open_filename;
+  startAndControl ("Open file..." + String (filename), open_filename = SD.open(String (filename), FILE_WRITE), count);
+  delay(20);
+
+  if(open_filename) {
+    open_filename.println(check_data);
+    open_filename.close();        
+  }
+
+  else {
+    #if defined LEDDEBUG
+      errorFlash(ERRORLED,count);        
+    #endif
+    #if defined DEBUGMODE
+      SerialDEBUG.println("Cannot writo to file"+String(filename));
+    #endif
+    return; 
+  }
+
+}
