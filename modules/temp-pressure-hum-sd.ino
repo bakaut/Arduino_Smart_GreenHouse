05af813f035a3cee651980627689ee0eaaead0a1
bakaut <fifo.mail@gmail.com>
2018-05-19 13:22:35 +0300

    Update temp-pressure-hum-sd.ino
    no any ckecks. just actions

--- modules/temp-pressure-hum-sd.ino
+++ modules/temp-pressure-hum-sd.ino
@@ -1,4 +1,3 @@
-//#include "Sleeper.h"
 #include <Time.h>
 #include <TimeLib.h>
 #include <DS1307RTC.h>
@@ -20,18 +19,7 @@
 #define PAUSE 500
 
 
-#define WRITESD
-//#define DEBUGMODE
-//#define LEDDEBUG
-
-//Serial port for logging. Use Hardware Serial  on Mega, or software serial for Nano
-#if defined DEBUGMODE
-#define SerialDEBUG Serial
-#endif
-
-#ifdef WRITESD
 const uint8_t SD_SC = 10; //SD card 
-#endif
 const uint8_t POWER_SWITCH = 5; //Turn on or turn off all sensor power via mosfet transistor
 const uint8_t ERRORLED = 13;
 
@@ -40,7 +28,7 @@ unsigned long pressure, aver_pressure, pressure_array[6], time_array[6];
 unsigned long sumX, sumY, sumX2, sumXY;
 int delta,current_day;
 float a,h,t,p,t_int;
-String check_data,check_data_prefix; //all sensor data in inflife line protocol
+String check_data_prefix; //all sensor data in inflife line protocol
 String all_data = "file.txt", current_data = "current.txt", current_day_file;
 File all_data_file, current_data_file, current_day_file_file;
 
@@ -49,23 +37,14 @@ File all_data_file, current_data_file, current_day_file_file;
 DHT dht(DHTPIN, DHT22);
 //Датчик давления
 Adafruit_BMP085 bmp;
-#ifdef WRITESD
 //SD Карта
 SdFat SD;
-#endif
 //Часы. Tyny RTC clock
 tmElements_t tm;
 
-//Sleeper g_sleeper;
-
 void setup() {
 
-  #if defined DEBUGMODE
-  SerialDEBUG.begin(9600);
-  delay(100);
-  #endif
-
-  //Включаем питание датчиков через транзистор
+ //Включаем питание датчиков через транзистор
   pinMode(POWER_SWITCH, OUTPUT);
 
   digitalWrite(POWER_SWITCH, HIGH);
@@ -78,9 +57,6 @@ void setup() {
   //Включаем датчик давления
   bmp.begin();
 
-  #if defined LEDDEBUG
-    errorFlash(ERRORLED,1);
-  #endif
 
   pressure = aver_sens();          // найти текущее давление по среднему арифметическому
   for (byte i = 0; i < 6; i++) {   // счётчик от 0 до 5
@@ -92,18 +68,16 @@ void setup() {
 
 void loop() {
 
-
   digitalWrite(POWER_SWITCH, HIGH);
   delay(500);
-#ifdef WRITESD
-  startAndControl ("Initializing sd...", SD.begin(SD_SC), 2);
+  SD.begin(SD_SC);
+
   //Read temperature
-#endif
-  startAndControl ("Read temperature from pressure sensor ...", bmp.readTemperature(), 3);
+  bmp.readTemperature();
   delay(100);
   
   //Read date time
-  startAndControl ("Read date time...", RTC.read(tm), 4);
+  RTC.read(tm);
   delay(700);
   current_day = tm.Day;
   current_day_file = String (current_day);
@@ -115,10 +89,6 @@ void loop() {
   t_int = bmp.readTemperature();
   
    
-  // Проверка удачно прошло ли считывание.
-  
-  //startAndControl ("Check readed data from sensors...", isnan(h) && isnan(t)&& isnan(p), 8);
-
   pressure = aver_sens();                          // найти текущее давление по среднему арифметическому
   for (byte i = 0; i < 5; i++) {                   // счётчик от 0 до 5 (да, до 5. Так как 4 меньше 5)
     pressure_array[i] = pressure_array[i + 1];     // сдвинуть массив давлений КРОМЕ ПОСЛЕДНЕЙ ЯЧЕЙКИ на шаг назад
@@ -144,29 +114,22 @@ void loop() {
   
   //140 symvol restriction. Make 2 String variable.
   check_data_prefix = "weather,location=uglovo,region=aerodrom ";
-  check_data = "te="+String (t)+",ti="+String (t_int)+",td="+String (t-t_int)+",h="+String (h)+",p="+String (p)+",pd="+String (delta)+" "+String (makeTime(tm))+"000000000";
+  const String check_data = "te="+String (t)+",ti="+String (t_int)+",td="+String (t-t_int)+",h="+String (h)+",p="+String (p)+",pd="+String (delta)+" "+String (makeTime(tm))+"000000000";
   
-  #if defined DEBUGMODE
-  SerialDEBUG.println(check_data);
-  #endif 
-  #ifdef WRITESD
   
-  write_to_sd (all_data, all_data_file, check_data, 9);
-
-  write_to_sd (current_day_file, current_data_file, check_data, 10);
-  
-  write_to_sd (current_data, current_day_file_file, check_data, 11);
+  write_to_sd (all_data, all_data_file, check_data, check_data_prefix);
+  write_to_sd (current_day_file, current_data_file, check_data, check_data_prefix);
+  write_to_sd (current_data, current_day_file_file, check_data, check_data_prefix);
 
   if ( tm.Hour == 23 and tm.Minute >45 ) {
-    startAndControl ("Remove current day file...", SD.remove("current.txt"), 12);
-     
+     SD.remove("current.txt");  
   }
-  #endif
+
   //turn off power via mosfet
   delay(2000);
   digitalWrite(POWER_SWITCH, LOW);
-  delay(5000);
-  //g_sleeper.SleepMillis(5000);  
+  delay(600000);//10 minutes
+
   
   }
 
@@ -181,71 +144,13 @@ long aver_sens() {
 }
 
 
-void flash (char led, unsigned short interval, unsigned short pause)
-{ while(1){
-  digitalWrite(led, HIGH);
-  delay(interval);
-  digitalWrite(led, LOW);
-  delay(pause); 
-}
- 
-  }
 
-void startAndControl (String message,boolean command,uint8_t count) {
-  #if defined DEBUGMODE
-  SerialDEBUG.print(message+"\r\n");
-  #endif
-  boolean state = true;
-  while (state) {
-    if (!command) {
-        #if defined DEBUGMODE
-        SerialDEBUG.println(" Failed");
-        #endif
-        #if defined LEDDEBUG
-          errorFlash(ERRORLED,count);        
-        #endif
-        return;
-       }
-    else {
-      state = false;
-      #if defined DEBUGMODE
-      SerialDEBUG.println(" OK");
-      #endif
-      } 
-   }
-  }
-
-void flash (uint8_t led, uint8_t count, unsigned short interval, unsigned short pause)
-{ 
-  uint8_t  i;
-  
-  for (i=0; i < count; i++) {
-      digitalWrite(led, HIGH);
-      delay(interval);
-      digitalWrite(led, LOW);
-      delay(pause);     
-    }
-  
- 
-  }
-
-void errorFlash (uint8_t errorled, uint8_t count)
-
-{   
-    flash(errorled,count,100,100);
-    digitalWrite(errorled, HIGH);
-    delay(2000);
-    digitalWrite(errorled, LOW);
-  }
-#ifdef WRITESD
-void write_to_sd (String filename, File file_var, String check_data, uint8_t count) {
-
-  startAndControl ("Open file..." + filename, file_var = SD.open(filename, FILE_WRITE), count);
+void write_to_sd (String filename, File file_var, String check_data, String check_data_prefix) {
+  file_var = SD.open(filename, FILE_WRITE);
   delay(100);
   file_var.print(check_data_prefix);
   file_var.position();
   file_var.println(check_data);
   file_var.close();
-  delay(200);      
+  delay(100);      
 }
-#endif
